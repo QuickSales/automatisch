665f19cecef13e66a6ebe5fa986c7e225a50d1bd
Ali BARIN <ali.barin53@gmail.com>
2023-01-15 16:10:53 +0100

    feat(filter): add filter app

---
+++ packages/backend/src/apps/filter/index.ts
@@ -0,0 +1,12 @@
+import defineApp from '../../helpers/define-app';
+
+export default defineApp({
+  name: 'Filter',
+  key: 'filter',
+  iconUrl: '{BASE_URL}/apps/filter/assets/favicon.svg',
+  authDocUrl: 'https://automatisch.io/docs/apps/filter/connection',
+  supportsConnections: false,
+  baseUrl: '',
+  apiBaseUrl: '',
+  primaryColor: '001F52',
+});
